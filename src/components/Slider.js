ffbace012cb0e5fde109bc9a6ce2ad2f2a4caa6c
Farzaneh-Mohammadi <mohammadifh8931@gmail.com>
2022-08-29 19:19:34 +0430

    responsive slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,12 +6,22 @@ import { useState } from "react";
 import styled from "styled-components";
 import { sliderItems } from "../data";
 
+import {mobile} from "./SliderResponsive"
+import {tablet} from "./SliderResponsive"
+
+
 const Container = styled.div`
   height: 60vh;
   display: flex;
   position: relative;
   overflow: hidden;
   margin: 2rem ;
+
+  ${mobile({ display: "none" })}
+  ${tablet({ 
+     height: "40vh",
+  margin: "1rem" ,
+   })}
 `;
 
 const Arrow = styled.div`
@@ -46,27 +56,50 @@ const Slide = styled.div`
   display: flex;
   align-items: center;
   background-color: #${(props) => props.bg};
+
+
+
 `;
 
 const ImgContainer = styled.div`
   height: 90%;
   flex: 1;
   margin: 3rem 5rem;
+
+  ${tablet({ 
+      height: "50%",
+      margin: " 0 5rem 16rem 5rem ",
+   })}
+
 `;
 
 const Image = styled.img`
   height: 65%;
   filter: drop-shadow(5px 5px 4px #6e706f);
+
+  ${tablet({ 
+  margin: "0 0 5rem 0",
+   })}
 `;
 
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
   margin: -15% 10rem 0 5rem;
+
+  ${tablet({ 
+   padding: "50px",
+  margin: "0 20rem 22rem 0"
+   })}
+   
 `;
 
 const Title = styled.h1`
   font-size: 50px;
+
+  ${tablet({ 
+  fontSize:" 30px",
+   })}
 `;
 
 const Desc = styled.p`
@@ -74,14 +107,15 @@ const Desc = styled.p`
   font-size: 20px;
   font-weight: 500;
   letter-spacing: 3px;
+
+  ${tablet({ 
+  fontSize:" 10px",
+  margin: "10px 0px",
+  letterSpacing:  "0",
+   })}
 `;
 
-const Button = styled.button`
-  padding: 10px;
-  font-size: 20px;
-  background-color: transparent;
-  cursor: pointer;
-`;
+
 
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
